a8680b7f52637ea4ada502f76141d95c0bac52e6
zhaosheng <1665471139@qq.com>
2023-09-12 19:04:00 +0800

    初始化提交

--- src/test/java/com/git/test/Test1.java
+++ src/test/java/com/git/test/Test1.java
@@ -1,24 +1,21 @@
-package com.git.test.run;
+package com.git.test;
 
 import com.git.test.config.AnnoConfig;
 import com.git.test.mapper.UserMapper;
 import com.git.test.po.User;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 /**
  * @author authorZhao
- * @since 2023-09-06
+ * @since 2023-09-08
  */
-public class TestSql {
-
-    public static void main(String[] args) {
-        //java -agentlib:native-image-agent=config-output-dir=E:\java\workspace\space3\mybatis-3\target -jar E:\java\workspace\space3\mybatis-3\target\mybatis-100-SNAPSHOT-jar-with-dependencies.jar
-
+public class Test1 {
+    @Test
+    public void test1(){
         UserMapper mapper = AnnoConfig.getSession().getMapper(UserMapper.class);
         List<User> list = mapper.list();
         System.out.println("list = " + list);
-
-
     }
 }
